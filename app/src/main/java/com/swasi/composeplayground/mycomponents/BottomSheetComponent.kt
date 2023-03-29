a2cb5816308d910551a49d7947b0ddbc91caaf01
siba-x-prasad <siba.x.prasad@gmail.com>
2023-03-29 22:46:18 +0100

    added bottomsheet

---
+++ app/src/main/java/com/swasi/composeplayground/mycomponents/BottomSheetComponent.kt
@@ -0,0 +1,31 @@
+package com.swasi.composeplayground.mycomponents
+
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.material.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Dp
+
+/**
+ * Created by Sibaprasad Mohanty on 29/03/2023.
+ * siba.x.prasad@gmail.com
+ */
+
+@Composable
+@ExperimentalMaterialApi
+fun ModalBottomSheetLayout(
+    sheetContent: @Composable ColumnScope.() -> Unit,
+    modifier: Modifier = Modifier,
+    sheetState: ModalBottomSheetState =
+        rememberModalBottomSheetState(ModalBottomSheetValue.Hidden),
+    sheetShape: Shape = MaterialTheme.shapes.large,
+    sheetElevation: Dp = ModalBottomSheetDefaults.Elevation,
+    sheetBackgroundColor: Color = MaterialTheme.colorScheme.surface,
+    sheetContentColor: Color = contentColorFor(sheetBackgroundColor),
+    scrimColor: Color = ModalBottomSheetDefaults.scrimColor,
+    content: @Composable () -> Unit
+) {
+}
